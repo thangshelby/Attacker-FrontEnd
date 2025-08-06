3dc83b50dac94ca63d7dd3b3653818353a3343de
baonmMDS <baonm@metadatasolutions.vn>
2025-08-06 22:06:17 +0700

    fix some bug from backend

--- src/apis/notification.js
+++ src/apis/notification.js
@@ -12,11 +12,10 @@ export const notification = {
   createNotification: (data) => api.post(`/notification`, data),
   updateNotification: (notification_id, data) =>
     api.patch(`notification/${notification_id}`, data),
-  markAllAsRead: (citizen_id) => {
+  markAllAsRead: (citizen_id) =>
     api.post("/notification/mark_all_as_read", {
       citizen_id,
-    });
-  },
+    }),
   deleteNotification: (notification_id) =>
     api.delete(`/notification/${notification_id}`),
 };
