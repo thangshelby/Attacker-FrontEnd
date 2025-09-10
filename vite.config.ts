82bac3088e0d5067626c0bbac3fd64d33bcd17ac
Bao Nguyen <baonm@metadatasolutions.vn>
2025-09-10 12:21:43 +0700

    updaeed

--- vite.config.ts
+++ vite.config.ts
@@ -30,6 +30,7 @@ export default defineConfig({
   },
   preview: {
     port: process.env.PORT || 4173,
-    host: '0.0.0.0'
+    host: '0.0.0.0',
+    allowedHosts: ['attacker-frontend-1.onrender.com', 'localhost']
   }
 });
